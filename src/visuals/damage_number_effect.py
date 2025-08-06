fec068c652d1936efdf0647926ae41c255183920
nilfm <nil.fons@gmail.com>
2025-08-06 16:44:48 -0400

    Make damage indicator float up as it disappears

--- src/visuals/damage_number_effect.py
+++ src/visuals/damage_number_effect.py
@@ -20,22 +20,31 @@ class DamageNumberEffect(VisualEffect):
             min_size: int = 20,
             max_size: int = 80,
     ) -> None:
-        super().__init__(2 if is_crit else 1)
+        super().__init__(self._get_duration(is_crit))
         self.ball = ball
         self.amount = amount
         self.is_crit = is_crit
         self.min_size = min_size
         self.max_size = max_size
 
+    @staticmethod
+    def _get_duration(is_crit: bool) -> int:
+        return 2 if is_crit else 1
+
     def _get_font_size(self, damage: int) -> int:
         return self.min_size + (self.max_size - self.min_size) * damage // 100
+
+    def _get_y_drift(self) -> int:
+        max_drift = 100
+        duration = self._get_duration(self.is_crit)
+        return duration * int(max_drift * (1 - self.timer_seconds / duration))
 
     @override
     def draw(self, display: Display) -> None:
         color = (255, 0, 0) if not self.is_crit else (255, 180, 0)
         font = pygame.font.SysFont('Arial', self._get_font_size(damage=self.amount), bold=True)
-        alpha = int(255 * (self.timer_seconds / 1.0))
+        alpha = int(255 * self.timer_seconds / self._get_duration(self.is_crit))
         text = f"-{self.amount}"
         pos_x = self.ball.body.position.x
-        pos_y = self.ball.body.position.y - self.ball.radius - 18
+        pos_y = self.ball.body.position.y - self.ball.radius - 18 - self._get_y_drift()
         display.draw_text(text, (pos_x, pos_y), font, color, alpha=alpha)
